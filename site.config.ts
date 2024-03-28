b9261cb98441dec4a2386a65e79285a93844c4d8
sunnchao <sunchaowang@outlook.com>
2024-03-28 22:04:53 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  // domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'api.wochirou.com',
   author: '',
 
   // open graph metadata (optional)
